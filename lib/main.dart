3948877c45fe24f9ebb97a2b541a968ddcb103c4
Rbn31 <robson.v8@hotmail.com>
2023-03-22 11:40:14 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            const Card(
-                child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100'),
-              subtitle: Text('1000'),
-            )),
-            const Card(
-                child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200'),
-              subtitle: Text('1000'),
-            )),
-            Card(
-                child: Column(
-              children: const [
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.monetization_on_sharp),
-                    title: Text('300'),
-                    subtitle: Text('100'),
-                  ),
-                )
-              ],
-            )),
-          ],
-        ),
+        body: ListaTransferencia(),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
@@ -44,4 +17,38 @@ void main() {
       ),
     ),
   );
+}
+
+class ListaTransferencia extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100'),
+              subtitle: Text('1000'),
+            )),
+        const Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200'),
+              subtitle: Text('1000'),
+            )),
+        Card(
+            child: Column(
+              children: const [
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.monetization_on_sharp),
+                    title: Text('3200'),
+                    subtitle: Text('100'),
+                  ),
+                )
+              ],
+            )),
+      ],
+    );
+  }
 }
